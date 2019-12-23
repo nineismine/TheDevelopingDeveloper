889b332dea325e1330f87f615f3f9a39f86408d0
Chad Torkkola <ctorkkola#gmail.com>
2019-12-23 10:33:58 -0700

    Add project files.

---
+++ HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿//using statements import libraries of code for us to use in our project in this case we are importing "System" which contains the definition for the Console methods
+using System;
+
+
+//our namspace is used to organize your code they can be named whatever you want
+namespace Hello_World
+{
+
+    
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
